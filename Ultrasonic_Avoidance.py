12f9155de37061f609fab2476c5a68078300e9af
Dream <service@sunfounder.com>
2016-10-14 07:33:38 +0000

    update

--- Ultrasonic_Avoidance.py
+++ Ultrasonic_Avoidance.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 '''
 **********************************************************************
-* Filename    : UltraSonic_Avoidance.py
-* Description : A module for SunFounder UltraSonic Avoidance
+* Filename    : Ultrasonic_Avoidance.py
+* Description : A module for SunFounder Ultrasonic Avoidance
 * Author      : Cavon
 * Brand       : SunFounder
 * E-mail      : service@sunfounder.com
@@ -13,12 +13,12 @@
 import time
 import RPi.GPIO as GPIO
 
-class UltraSonic_Avoidance(object):
+class Ultrasonic_Avoidance(object):
 	def __init__(self, channel):
 		self.channel = channel
 		GPIO.setmode(GPIO.BCM)
 
-	def get_distance(self, Ranges=500):
+	def get_distance(self):
 		pulse_end = 0
 		pulse_start = 0
 		GPIO.setup(self.channel,GPIO.OUT)
@@ -28,6 +28,7 @@ class UltraSonic_Avoidance(object):
 		time.sleep(0.00001)
 		GPIO.output(self.channel, False)
 		GPIO.setup(self.channel,GPIO.IN)
+		
 		while GPIO.input(self.channel)==0:
 			pulse_start = time.time()
 		while GPIO.input(self.channel)==1:
@@ -37,19 +38,44 @@ class UltraSonic_Avoidance(object):
 			pulse_duration = pulse_end - pulse_start
 			distance = pulse_duration * 100 * 343.0 /2
 			distance = int(distance)
-			
-			if distance <= Ranges:
-				#print 'start = %s'%pulse_start,
-				#print 'end = %s'%pulse_end
-
+			#print 'start = %s'%pulse_start,
+			#print 'end = %s'%pulse_end
+			if distance >= 0:
 				return distance
+			else:
+				return -1
+		else :
+			#print 'start = %s'%pulse_start,
+			#print 'end = %s'%pulse_end
+			return -1
+
+	def less_than(self, alarm_gate):
+		dis = self.get_distance()
+		status = 0
+		if dis >=0 and dis <= alarm_gate:
+			status = 1
+		elif dis > alarm_gate:
+			status = 0
+		else:
+			status = -1
+		#print 'distance =',dis
+		#print 'status =',status
+		return status
 
 if __name__ == '__main__':
-	UA = UltraSonic_Avoidance(17)
+	UA = Ultrasonic_Avoidance(17)
+	threshold = 10
 	while True:
 		distance = UA.get_distance()
-		if distance >= 0:
+		status = UA.less_than(threshold)
+		if distance != -1:
 			print 'distance', distance, 'cm'
 			time.sleep(0.2)
-		elif distance == None:
+		else:
 			print False
+		if status == 1:
+			print "Less than %d" % threshold
+		elif status == 0:
+			print "Over %d" % threshold
+		else:
+			print "Read distance error."
